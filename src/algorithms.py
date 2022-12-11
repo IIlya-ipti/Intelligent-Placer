57a89a4117dff1d5a25a904c3e06b90476175530
IIlya-ipti <79226730+IIlya-ipti@users.noreply.github.com>
2022-12-11 11:36:27 +0300

    update alg

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,26 +1,26 @@
 import numpy as np
 
 
-
 def max_rect(a):
     v = [0 for i in range(len(a))]
     stack = []
     for i in range(len(a)):
         if len(stack) == 0:
-            stack.append((a[i],i))
+            stack.append((a[i], i))
         else:
             j = len(stack) - 1
-            while(j >= 0 and stack[j][0] > a[i]):
+            while (j >= 0 and stack[j][0] > a[i]):
                 v[stack[j][1]] += i - stack[j][1]
                 stack.pop()
                 j -= 1
-            stack.append((a[i],i))
+            stack.append((a[i], i))
     j = len(stack) - 1
-    while(len(stack) != 0):
+    while (len(stack) != 0):
         v[stack[j][1]] += i - stack[j][1] + 1
         stack.pop()
         j -= 1
     return v
+
 
 def total_max_rect(a):
     first = max_rect(a)
@@ -29,18 +29,15 @@ def total_max_rect(a):
         first[i] += second[i] - 1
     return np.array(first)
 
-def add_block(wid,hi,polygon):
-    
-    
+
+def add_block(wid, hi, polygon):
     # MAIN ALG FOR ADD BLOCK
     polygon_width = polygon.copy()
-    for j in range(polygon.shape[1] - 1,-1,-1):
-        for i in range(0,polygon.shape[0],1):
+    for j in range(polygon.shape[1] - 1, -1, -1):
+        for i in range(0, polygon.shape[0], 1):
             if polygon_width[i][j] != 0:
                 polygon_width[i][j] = polygon_width[i - 1][j] + polygon_width[i][j]
 
-    
-    
     for i in range(polygon_width.shape[0]):
 
         polygon_one = polygon_width[i]
@@ -55,17 +52,17 @@ def add_block(wid,hi,polygon):
         vls_second = vls_second[vls_second >= wid]
 
         if len(vls_first) > 0:
-            for i_1 in range(i,i - wid,-1):
+            for i_1 in range(i, i - wid, -1):
                 ind = np.where(polygon_two == vls_first[0])[0][0]
-                for j_1 in range(ind,ind + hi - 1):
-                     if i_1 >= 0 and i_1 < len(polygon) and j_1 >= 0 and j_1 < len(polygon[0]):
+                for j_1 in range(ind, ind + hi - 1):
+                    if i_1 >= 0 and i_1 < len(polygon) and j_1 >= 0 and j_1 < len(polygon[0]):
                         polygon[i_1][j_1] = 0
 
 
         elif len(vls_second) > 0:
-             ind = np.where(polygon_two == vls_second[0])[0][0]   
-             for i_1 in range(i,i - hi,-1):
-                for j_1 in range(ind,ind + wid - 1):
+            ind = np.where(polygon_two == vls_second[0])[0][0]
+            for i_1 in range(i, i - hi, -1):
+                for j_1 in range(ind, ind + wid - 1):
                     if i_1 >= 0 and i_1 < len(polygon) and j_1 >= 0 and j_1 < len(polygon[0]):
                         polygon[i_1][j_1] = 0
         if len(vls_first) + len(vls_second) > 0:
